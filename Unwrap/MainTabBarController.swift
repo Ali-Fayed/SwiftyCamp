c7fda086a150e776ec0417b3b8e930c15ccda4f9
Ali Fayed <ali.fayed2666@gmail.com>
2021-12-04 15:49:49 +0200

    changes

---
+++ Unwrap/MainTabBarController.swift
@@ -0,0 +1,38 @@
+//
+//  MainTabBarController.swift
+//  Unwrap
+//
+//  Created by Paul Hudson on 09/08/2018.
+//  Copyright © 2019 Hacking with Swift.
+//
+
+import UIKit
+
+/// A UITabBarController subclass that sets up our main coordinators as each of the five app tabs.
+class MainTabBarController: UITabBarController, Storyboarded {
+    let home = HomeCoordinator()
+    let learn = LearnCoordinator()
+    let practice = PracticeCoordinator()
+    let challenges = ChallengesCoordinator()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        viewControllers = [home.navigationController, learn.splitViewController, practice.splitViewController, challenges.splitViewController]
+    }
+
+    /// If we get some launch options, figure out which one was requested and jump right to the correct tab.
+    func handle(_ launchOptions: [UIApplication.LaunchOptionsKey: Any]?) {
+        if let item = launchOptions?[.shortcutItem] as? UIApplicationShortcutItem {
+            handle(shortcutItem: item)
+        }
+    }
+
+    func handle(shortcutItem: UIApplicationShortcutItem) {
+        if shortcutItem.type == "com.hackingwithswift.unwrapswift.challenges" {
+            selectedViewController = challenges.splitViewController
+        } else {
+            fatalError("Unknown shortcut item type: \(shortcutItem.type).")
+        }
+    }
+}
---
+++ Unwrap/User/User-Progress.swift
@@ -0,0 +1,13 @@
+//
+//  User-Progress.swift
+//  Unwrap
+//
+//  Created by Wayne Nihart on 4/27/19.
+//  Copyright © 2019 Hacking with Swift. All rights reserved.
+//
+
+import UIKit
+
+extension User {
+
+}
